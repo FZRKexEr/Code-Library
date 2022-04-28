e89d4528c5a8d7bcce51d69ad962af403f59bd5d
LiZnB <wrong.answer@qq.com>
2022-04-28 20:21:26 +0800

    Update

---
+++ Treap/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2022-04-28 20:09
+//
+//  Created by liznb
+//  
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false); 
+  cin.tie(0);
+
+
+  return 0;
+}
+
