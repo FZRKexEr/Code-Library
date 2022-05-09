b3f857528592be0659884c0d7849c6f362f394ba
LiZnB <wrong.answer@qq.com>
2022-05-09 17:01:55 +0800

    Update

---
+++ 线段树/main.cpp
@@ -0,0 +1,124 @@
+//
+//  main.cpp
+//  2022-05-09 17:00
+//
+//  Created by liznb
+//  
+#include <bits/stdc++.h>
+using namespace std;
+
+// 先build！先build！先build！
+// 修改：区间加法
+// 查询：区间和，最大值，最小值
+// 1e6 很可能 MLE，一定要修改 N
+// 修改模板顺序：
+// 0. 如果是单点修改，一定是先删除 lazy 数组 和 push_down
+// 1. Info 结构体
+// 2. build 函数
+// 3. merge 函数
+// 4. add 函数
+// 5. push_down 函数 
+// modify 和 query 不需要修改！
+
+const int N = 1e6 + 10;
+struct Segment_Tree {
+  
+  #define tl tree[pos].l 
+  #define tr tree[pos].r 
+  #define tv tree[pos].val
+  struct Info {
+    long long sum, maxi, mini; 
+  };
+
+  struct Tree {
+    int l, r; 
+    Info val;
+  } tree[4 * N];  
+  long long lazy[4 * N];
+
+  // 合并两个儿子
+  Info merge(Info a, Info b) {
+    Info res;
+    res.sum = a.sum + b.sum;
+    res.maxi = max(a.maxi, b.maxi);
+    res.mini = min(a.mini, b.mini);
+    return res;
+  }
+
+  void build(int pos, int l, int r, long long *arr) {
+    tl = l; tr = r;    
+    lazy[pos] = 0ll;
+
+    int m = l + (r - l) / 2;
+    if (l == r) {
+      tv.sum = arr[l]; 
+      tv.maxi = arr[l];
+      tv.mini = arr[l];
+      return;
+    } 
+    build(pos << 1, l, m, arr);
+    build(pos << 1 | 1, m + 1, r, arr);
+    tv = merge(tree[pos << 1].val, tree[pos << 1 | 1].val);
+  }
+
+  // 对树上一个位置进行修改
+  void add(int pos, long long val) {
+    lazy[pos] += val;
+    tv.sum += (tr - tl + 1) * val;
+    tv.maxi += val;
+    tv.mini += val;
+  }
+
+  // 传递懒人标记
+  void push_down(int pos) {
+    if (lazy[pos] == 0) return; 
+    add(pos << 1, lazy[pos]); 
+    add(pos << 1 | 1, lazy[pos]);
+    lazy[pos] = 0;
+  } 
+
+  void modify(int pos, int l, int r, long long val) {
+    if (tl >= l && tr <= r) {
+      add(pos, val);
+      return;
+    }  
+    push_down(pos);
+    int m = tl + (tr - tl) / 2;
+    if (l <= m) modify(pos << 1, l, r, val);
+    if (r > m) modify(pos << 1 | 1, l, r, val);
+    tv = merge(tree[pos << 1].val, tree[pos << 1 | 1].val);
+  } 
+
+  Info query(int pos, int l, int r) {
+    if (tl >= l && tr <= r) {
+      return tv;
+    }   
+    push_down(pos);
+    int m = tl + (tr - tl) / 2;
+    Info res;
+
+    if (l <= m && r > m) {
+      res = merge(query(pos << 1, l, r), query(pos << 1 | 1, l , r));
+    } else if (l > m) {
+      res = query(pos << 1 | 1, l, r);
+    } else if (r <= m) {
+      res = query(pos << 1, l, r);
+    }
+
+    tv = merge(tree[pos << 1].val, tree[pos << 1 | 1].val);
+    return res;
+  }
+  #undef tl
+  #undef tr
+  #undef tv
+} ST;
+
+int main() {
+  ios::sync_with_stdio(false); 
+  cin.tie(0);
+  
+
+  return 0;
+}
+
+
