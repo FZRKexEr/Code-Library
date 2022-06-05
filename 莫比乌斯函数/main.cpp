c32aef9c8994e5b7785566719531eb1ed9b71e2c
LiZnB <wrong.answer@qq.com>
2022-06-05 14:08:37 +0800

    Update

---
+++ 莫比乌斯函数/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  2022-06-05 13:53
+//
+//  Created by liznb
+//  
+#include <bits/stdc++.h>
+using namespace std;
+
+// 说明:
+// 1. is = 0 时是质数
+// 2. 线性筛：利用莫比乌斯函数的积性
+struct Mobius {
+  vector<int> is, prinum, miu;
+
+  Mobius(int n) : is(n + 1, 0), prinum(n + 1), miu(n + 1, 1) {
+    for (int i = 2; i <= n; i++) {
+      if (!is[i]) prinum.push_back(i), miu[i] = -1;  
+      for (auto &it : prinum) {
+        if (it * i > n) break;
+        is[it * i] = 1;
+        if (i % it == 0) {
+          miu[it * i] = 0;
+          break;
+        } else {
+          miu[it * i] = -1 * miu[i];
+        }
+      }
+    }
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false); 
+  cin.tie(0);
+
+  Mobius T(100);
+  for (int i = 1; i <= 100; i++) {
+    cout << T.miu[i] << " ";  
+  }
+
+  return 0;
+}
+
---
+++ 莫比乌斯反演/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  2022-06-05 13:51
+//
+//  Created by liznb
+//  
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false); 
+  cin.tie(0);
+  
+   
+
+  return 0;
+}
+
