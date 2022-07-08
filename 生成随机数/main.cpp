107fc4d4a41139e6832c908bdefd025e65071f0c
LiZnB <wrong.answer@qq.com>
2022-07-08 20:42:39 +0800

    Update

---
+++ 生成随机数/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  2022-07-08 20:41
+//
+//  Created by liznb
+//  
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<long long> U(1, 1000000000ll);
+
