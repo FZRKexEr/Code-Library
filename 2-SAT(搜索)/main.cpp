31e11098881f58dbdd643156d88fb2ceb6b327bc
LiZnB <wrong.answer@qq.com>
2022-08-08 03:09:01 +0800

    Update

--- 2-SAT(搜索)/main.cpp
+++ 2-SAT(搜索)/main.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 // 标准 2-SAT
 // 暴搜 + 随机化
-// 最坏复杂度 O(nm), 随机化后很难被卡（怀疑甚至不能被卡）。
-// 在随机数据下效率非常优秀。
+// 期望复杂度 O(nlogn ?), 实测效率优秀。
 // 下标从1开始。
+// 注意：2-sat 很简单，如果不赶时间建议理解代码再粘板子
 
 struct TwoSAT{
   vector<vector<int>> G;
@@ -23,6 +23,7 @@ struct TwoSAT{
 
   TwoSAT(int _n) : G(_n * 2 + 2), vis(_n * 2 + 2, 0) { n = _n; }
  
+  // a = at, b = bt 至少一个成立
   void addor(int a, int at, int b, int bt) {
     a += a + at;
     b += b + bt;
@@ -30,7 +31,8 @@ struct TwoSAT{
     G[b ^ 1].push_back(a); 
   }
 
-  void addxor(int a, int at, int b, int bt) {
+  // a = at, b = bt 不能同时成立
+  void addnand(int a, int at, int b, int bt) {
     a += a + at;
     b += b + bt;
     G[a].push_back(b ^ 1);
@@ -54,17 +56,17 @@ struct TwoSAT{
     vector<int> ord(n + 1);
     iota(ord.begin(), ord.end(), 0);
     shuffle(ord.begin() + 1, ord.end(), seed); // 随机选点 dfs
-    
     for (int i = 1; i <= n; i++) {
       int it = ord[i];
       if (!vis[it * 2 + 1] && !vis[it * 2]) {
         while (!stk.empty()) stk.pop();
-        if (!dfs(it * 2)) {
+        int c = (it ^ i) & 1; // 随机选真假
+        if (!dfs(it * 2 + c)) {
           while (!stk.empty()) {
             vis[stk.top()] = 0;
             stk.pop();
           }
-          if (!dfs(it * 2 + 1)) return false;
+          if (!dfs(it * 2 + (c ^ 1))) return false;
         }
       }
     }
@@ -83,6 +85,7 @@ signed main() {
     int u, x, v, y; cin >> u >> x >> v >> y;
     T.addor(u, x, v, y);
   }
+
   if (T.sol()) {
     cout << "POSSIBLE" << endl;
     for (int i = 1; i <= n; i++) {
