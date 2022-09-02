47991bcd4b4c7b5567d82f6d3c411ecd2615fbe8
LiZnB <wrong.answer@qq.com>
2022-09-02 17:20:00 +0800

    Update

--- Link-Cut Tree/main.cpp
+++ Link-Cut Tree/main.cpp
@@ -7,13 +7,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<class T>
 struct LCT {
+  struct Node {
+    array<int, 2> son;
+    int fa, lazy, cnt, size;
+    T val;
+    Node() : son({0, 0}) { fa = lazy = 0; cnt = size = 1; }
+  };
   
+
 };
 
 int main() {
-  ios::sync_with_stdio(false); 
+  ios::sync_with_stdio(false);
   cin.tie(0);
+
+  LCT<int> T;
 
   return 0;
 }
