0d62f6fea73d4b373b1e12bf50148f44d312a0dc
LiZnB <wrong.answer@qq.com>
2022-12-01 17:37:26 +0800

    Update

--- 线段树mini/main.cpp
+++ 线段树mini/main.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 const long long INF = 1e18;
 struct Sgt_Sum {
-  using node = long long;
-  vector<node> tree, lazy;
+  using Node = long long;
+  vector<Node> tree, lazy;
   Sgt_Sum(int n) {
     tree.resize(4 * (n + 1), 0);
     lazy.resize(4 * (n + 1), 0);
@@ -62,64 +62,79 @@ struct Sgt_Sum {
   }
 };
 
-// 线段树mini 求最大值
-struct Sgt_Max {
-  using node = long long;
-  vector<node> tree, lazy;
-  Sgt_Max(int n) {
-    tree.resize(4 * (n + 1), 0);
+// 线段树mini 求最大值/最小值
+struct Sgt_MinMax {
+  struct Node {
+    long long maxn, minn;
+  };
+  vector<Node> tree;
+  vector<long long> lazy;
+  Sgt_MinMax(int n) {
+    tree.resize(4 * (n + 1), {0, 0});
     lazy.resize(4 * (n + 1), 0);
   }
   void push_down(int pos) {
     if (lazy[pos]) {
       lazy[pos << 1] += lazy[pos];
       lazy[pos << 1 | 1] += lazy[pos];
-      tree[pos << 1] += lazy[pos];
-      tree[pos << 1 | 1] += lazy[pos];
+      tree[pos << 1].maxn += lazy[pos];
+      tree[pos << 1 | 1].maxn += lazy[pos];
+      tree[pos << 1].minn += lazy[pos];
+      tree[pos << 1 | 1].minn += lazy[pos];
       lazy[pos] = 0;
     }
   }
-  void build(int pos, int tl, int tr) {
+  void build(int pos, int tl, int tr, vector<long long> &arr) {
     if (tl == tr) {
-      cin >> tree[pos];
+      tree[pos] = {arr[tl], arr[tl]};
       return;
     }
     int m = (tl + tr) / 2;
-    build(pos << 1, tl, m);
-    build(pos << 1 | 1, m + 1, tr);
-    tree[pos] = max(tree[pos << 1], tree[pos << 1 | 1]);
+    build(pos << 1, tl, m, arr);
+    build(pos << 1 | 1, m + 1, tr, arr);
+    tree[pos].minn = min(tree[pos << 1].minn, tree[pos << 1 | 1].minn);
+    tree[pos].maxn = max(tree[pos << 1].maxn, tree[pos << 1 | 1].maxn);
   }
   void modify(int l, int r, long long val, int pos, int tl, int tr) {
     if (tl >= l && tr <= r) {
       lazy[pos] += val;
-      tree[pos] += val;
+      tree[pos].maxn += val;
+      tree[pos].minn += val;
       return;
     }
     push_down(pos);
     int m = (tl + tr) / 2;
     if (l <= m) modify(l, r, val, pos << 1, tl, m);
     if (r > m) modify(l, r, val, pos << 1 | 1, m + 1, tr);
-    tree[pos] = max(tree[pos << 1], tree[pos << 1 | 1]);
+    tree[pos].minn = min(tree[pos << 1].minn, tree[pos << 1 | 1].minn);
+    tree[pos].maxn = max(tree[pos << 1].maxn, tree[pos << 1 | 1].maxn);
   }
-  long long query(int l, int r, int pos, int tl, int tr) {
+  Node query(int l, int r, int pos, int tl, int tr) {
     if (tl >= l && tr <= r) return tree[pos];
     push_down(pos);
     int m = (tl + tr) / 2;
-    long long res = -INF;
-    if (l <= m) res = max(res, query(l, r, pos << 1, tl, m));
-    if (r > m) res = max(res, query(l, r, pos << 1 | 1, m + 1, tr));
-    tree[pos] = max(tree[pos << 1], tree[pos << 1 | 1]);
-    return res;
+    long long res_min = INF, res_max = -INF;
+    if (l <= m) {
+      Node res = query(l, r, pos << 1, tl, m);
+      res_max = max(res_max, res.maxn);
+      res_min = min(res_min, res.minn);
+    }
+    if (r > m) {
+      Node res = query(l, r, pos << 1 | 1, m + 1, tr);
+      res_max = max(res_max, res.maxn);
+      res_min = min(res_min, res.minn);
+    }
+    return {res_max, res_min};
   }
   // 线段树二分
-  int find_first_knowingly(int pos, int tl, int tr, const function<bool(const node&)> &f) {
+  int find_first_knowingly(int pos, int tl, int tr, const function<bool(const Node&)> &f) {
     if (tl == tr) return tl;
     push_down(pos);
     int m = (tl + tr) / 2;
     if (f(tree[pos << 1])) return find_first_knowingly(pos << 1, tl, m, f);
     else return find_first_knowingly(pos << 1 | 1, m + 1, tr, f);
   }
-  int find_first(int l, int r, int pos, int tl, int tr, const function<bool(const node&)> &f) {
+  int find_first(int l, int r, int pos, int tl, int tr, const function<bool(const Node&)> &f) {
     if (tl >= l && tr <= r) {
       if (!f(tree[pos])) return -1;
       return find_first_knowingly(pos, tl, tr, f);
@@ -132,12 +147,32 @@ struct Sgt_Max {
     if (r > m && res == -1) res = find_first(l, r, pos << 1 | 1, m + 1, tr, f);
     return res;
   }
+  int find_last_knowingly(int pos, int tl, int tr, const function<bool(const Node&)> &f) {
+    if (tl == tr) return tl;
+    push_down(pos);
+    int m = (tl + tr) / 2;
+    if (f(tree[pos << 1 | 1])) return find_last_knowingly(pos << 1 | 1, m + 1, tr, f);
+    else return find_last_knowingly(pos << 1, tl, m, f);
+  }
+  int find_last(int l, int r, int pos, int tl, int tr, const function<bool(const Node&)> &f) {
+    if (tl >= l && tr <= r) {
+      if (!f(tree[pos])) return -1;
+      return find_last_knowingly(pos, tl, tr, f);
+    }
+    push_down(pos);
+    int m = (tl + tr) / 2;
+    int res = -1;
+
+    if (r > m) res = find_last(l, r, pos << 1 | 1, m + 1, tr, f);
+    if (l <= m && res == -1) res = find_last(l, r, pos << 1, tl, m, f);
+    return res;
+  }
 };
 
 // 线段树mini 单点修改，区间查询最大值
 struct Sgt {
-  using node = long long;
-  vector<node> tree;
+  using Node = long long;
+  vector<Node> tree;
   Sgt(int n) {
     tree.resize(4 * (n + 1), 0);
   }
@@ -171,13 +206,13 @@ struct Sgt {
   }
 
   // 线段树二分，注意单点修改没有 push_down, 区间修改需要加上
-  int find_first_knowingly(int pos, int tl, int tr, const function<bool(const node&)> &f) {
+  int find_first_knowingly(int pos, int tl, int tr, const function<bool(const Node&)> &f) {
     if (tl == tr) return tl;
     int m = (tl + tr) / 2;
     if (f(tree[pos << 1])) return find_first_knowingly(pos << 1, tl, m, f);
     else return find_first_knowingly(pos << 1 | 1, m + 1, tr, f);
   }
-  int find_first(int l, int r, int pos, int tl, int tr, const function<bool(const node&)> &f) {
+  int find_first(int l, int r, int pos, int tl, int tr, const function<bool(const Node&)> &f) {
     if (tl >= l && tr <= r) {
       if (!f(tree[pos])) return -1;
       return find_first_knowingly(pos, tl, tr, f);
@@ -189,21 +224,44 @@ struct Sgt {
     if (r > m && res == -1) res = find_first(l, r, pos << 1 | 1, m + 1, tr, f);
     return res;
   }
+  int find_last_knowingly(int pos, int tl, int tr, const function<bool(const Node&)> &f) {
+    if (tl == tr) return tl;
+    int m = (tl + tr) / 2;
+    if (f(tree[pos << 1 | 1])) return find_last_knowingly(pos << 1 | 1, m + 1, tr, f);
+    else return find_last_knowingly(pos << 1, tl, m, f);
+  }
+  int find_last(int l, int r, int pos, int tl, int tr, const function<bool(const Node&)> &f) {
+    if (tl >= l && tr <= r) {
+      if (!f(tree[pos])) return -1;
+      return find_last_knowingly(pos, tl, tr, f);
+    }
+    int m = (tl + tr) / 2;
+    int res = -1;
+
+    if (r > m) res = find_last(l, r, pos << 1 | 1, m + 1, tr, f);
+    if (l <= m && res == -1) res = find_last(l, r, pos << 1, tl, m, f);
+    return res;
+  }
 };
 
 signed main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int n, m; cin >> n >> m;
+  int l = 2, r = 4, n = 5;
 
-  Sgt_Max T(n);
-  T.build(1, 1, n);
-  mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+  Sgt_MinMax T(n);
+  vector<long long> a = {0, 1, 2, 3, 4, 5};
+  T.build(1, 1, n, a);
 
-  for (int i = 1; i <= m; i++) {
-    int l, r; cin >> l >> r;
-    cout << T.query(l, r, 1, 1, n) << endl;
-  }
+  // 找到 l, r 区间内第一个大于 2 的数
+  // f 的意义是判断左/右区间能否进入
+  int p = T.find_first(l, r, 1, 1, n, [&](const Sgt_MinMax::Node& it) {
+    if (it.maxn > 2) return true;
+    return false;
+  });
+
+  cout << p << endl;
+
   return 0;
 }
