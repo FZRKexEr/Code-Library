f1639642e2e12d78f0a2be342f2871a0743f9211
LiZnB <wrong.answer@qq.com>
2022-12-02 13:48:40 +0800

    Update

--- 权值线段树(动态开点)/main.cpp
+++ 权值线段树(动态开点)/main.cpp
@@ -12,113 +12,131 @@ using namespace std;
 /*
  * 关于权值线段树:
  * 1. 经典问题：求数列第K大（区间第K大用可持久化线段树）
+ *
  * 关于动态开点：
  * 1. 能不用就不用，能离散化就离散化
  * 2. 已知用处：可持久化数据结构，线段树合并，强制在线不能离散化的题。
+ *
  * 模板特性：
- * 1. 只支持单点修改，区间查询。(反过来也行，不支持lazy标记)
- * 2. 如果问题能用普通线段树解决，就不要用这个。空间复杂度 Knlogn（K是大常数）
- * 3. 支持负数权值
- * 4. 不用 #define int long long 可以保证 long long 内结果正确
- * 5. !!!!! pos 是 0-indexed !!!!
- * 6. !!!!!! 单点覆盖时，注意这个点存不存在！，存在时才能用 -old_val + new_val 覆盖,否则直接 new_val
+ * 1. 支持区间修改，区间查询。
+ * 2. 支持负数权值
+ *
  * 优化：
  * 1. 模板内置了 sum，maxn，minn。可以根据题目要求删除一些，减少空间消耗。
  * 2. 还可以进一步优化空间：把 l 和 r 也删掉，直接在函数里传参
  * 3. 参考：逆序对问题：n = 5e5, 数值范围 1e9, 做了1，2优化后消耗 75mb 空间。
+ *
+ * 注意:
+ * 1. !!!!! pos 是 0-indexed !!!!
+ * 2. 如果问题能用普通线段树解决，就不要用这个。空间复杂度 Knlogn（K是大常数）
+ * 3. 先删除不用的变量，否则很容易 MLE
+ * 4. lazy 用 pair 是为了保证区间覆盖成 0 也没问题
  */
 
-const long long INF = 0x3f3f3f3f3f3f3f3f;
 struct Segment_Tree {
 
   struct Info {
-    long long sum, maxn, minn; 
-    Info () { sum = 0, maxn = 0, minn = 0; }
+    int sum;
+    Info () { sum = 0; }
   };
 
   struct Tree {
     int ls, rs; // 左儿子，右儿子 编号
-    int l, r; // 左边界，右边界
     Info dat;
-    Tree(int a, int b, int c, int d) : ls(a), rs(b), l(c), r(d) {} 
+    Tree(int a, int b) : ls(a), rs(b) {}
   };
 
-  vector<Tree> tree; 
+  vector<Tree> tree;
+  vector<pair<int, bool>> lazy;
 
-  Segment_Tree(int l, int r) {
-    tree.push_back({-1, -1, l, r});
+  int il, ir;
+
+  Segment_Tree(int l, int r) : il(l), ir(r) {
+    tree.emplace_back(-1, -1);
+    lazy.emplace_back(0, 0);
   }
 
-  int insert(int l, int r) {
-    tree.push_back({-1, -1, l, r});
+  int insert() {
+    tree.emplace_back(-1, -1);
+    lazy.emplace_back(0, 0);
     return (int) tree.size() - 1;
   }
- 
+
   Info merge(Info a, Info b) {
     Info res;
     res.sum = a.sum + b.sum;
-    res.maxn = max(a.maxn, b.maxn);
-    res.minn = min(a.minn, b.minn);
     return res;
   }
 
-  void add(int pos, long long val) {
-    tree[pos].dat.sum += val;
-    tree[pos].dat.maxn += val;
-    tree[pos].dat.minn += val;
+  void add(int pos, int tl, int tr, long long val) {
+    tree[pos].dat.sum = val * (tr - tl + 1);
+    lazy[pos] = {val, 1};
   }
 
   void push_up(int pos) {
-    tree[pos].dat.sum = 0; 
-    tree[pos].dat.maxn = -INF;
-    tree[pos].dat.minn = INF;
+    tree[pos].dat.sum = 0;
     if (tree[pos].ls != -1) tree[pos].dat = merge(tree[pos].dat, tree[tree[pos].ls].dat);
     if (tree[pos].rs != -1) tree[pos].dat = merge(tree[pos].dat, tree[tree[pos].rs].dat);
   }
 
-  void modify(int pos, int goal, long long val) {
-    if (tree[pos].l == tree[pos].r) {
-      add(pos, val);
+  void push_down(int pos, int tl, int tr) {
+    if (lazy[pos].second) {
+      int mid = (tl + tr) >> 1;
+      if (tree[pos].ls == -1) tree[pos].ls = insert();
+      if (tree[pos].rs == -1) tree[pos].rs = insert();
+      add(tree[pos].ls, tl, mid, lazy[pos].first);
+      add(tree[pos].rs, mid + 1, tr, lazy[pos].first);
+      lazy[pos] = {0, 0};
+    }
+  }
+
+  void modify(int pos, int tl, int tr, int l, int r, long long val) {
+    if (tl >= l && tr <= r) {
+      add(pos, tl, tr, val);
       return;
     }
-    int mid = (tree[pos].l + tree[pos].r) >> 1;
-    if (goal <= mid) {
-      if (tree[pos].ls == -1) tree[pos].ls = insert(tree[pos].l, mid);
-      modify(tree[pos].ls, goal, val);
-    } else {
-      if (tree[pos].rs == -1) tree[pos].rs = insert(mid + 1, tree[pos].r);
-      modify(tree[pos].rs, goal, val);
+    push_down(pos, tl, tr);
+    int mid = (tl + tr) >> 1;
+    if (l<= mid) {
+      if (tree[pos].ls == -1) tree[pos].ls = insert();
+      modify(tree[pos].ls, tl, mid, l, r, val);
+    }
+    if (r > mid) {
+      if (tree[pos].rs == -1) tree[pos].rs = insert();
+      modify(tree[pos].rs, mid + 1, tr, l, r, val);
     }
     push_up(pos);
   }
 
-  Info query(int pos, int l, int r) {
-    if (tree[pos].l >= l && tree[pos].r <= r) {
+  Info query(int pos, int tl, int tr, int l, int r) {
+    if (tl >= l && tr <= r) {
       return tree[pos].dat;
     }
-    int mid = (tree[pos].l + tree[pos].r) >> 1;
-    Info res; res.maxn = -INF, res.minn = INF; //注意初始值
+
+    push_down(pos, tl, tr);
+    int mid = (tl + tr) >> 1;
+    Info res;
 
     if (l <= mid && tree[pos].ls != -1) {
-      res = merge(res, query(tree[pos].ls, l, r));
+      res = merge(res, query(tree[pos].ls, tl, mid, l, r));
     }
     if (r >= mid + 1 && tree[pos].rs != -1) {
-      res = merge(res, query(tree[pos].rs, l, r));
+      res = merge(res, query(tree[pos].rs, mid + 1, tr, l, r));
     }
     push_up(pos);
     return res;
   }
 
-  void modify(int goal, long long val) { modify(0, goal, val); }
-  Info query(int l, int r) { return query(0, l, r); }
+  void modify(int l, int r, long long val) { modify(0, il, ir, l, r, val); }
+  Info query(int l, int r) { return query(0, il, ir, l, r); }
 };
+
 
 signed main() {
   ios::sync_with_stdio(false); 
   cin.tie(0);
 
   Segment_Tree T(1, 1e9); 
-
   
   return 0;
 }
